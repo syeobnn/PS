71c4536166440ff6bc02882ab74de394be91fabe
syeobnn <48374788+syeobnn@users.noreply.github.com>
2025-01-16 14:08:24 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 120 ms, Memory: 11796 KB -BaekjoonHub

---
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.cc
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cstring>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+
+#define ll long long
+#define pii pair<int, int>
+#define MAX 1e9
+#define log2 18
+
+using namespace std;
+
+const int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+const int ddx[] = {1, -1, 0, 0};
+const int ddy[] = {0, 0, -1, 1};
+const int INF = 987654321;
+const ll LINF = 3123372036854775807;
+
+inline void fastIo() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+vector<pii > start;
+vector<pii > endi;
+int arr[1025][1025];
+int dp[1025][1025];
+
+int main() {
+    fastIo();
+
+    int N, T;
+
+    cin >> N >> T;
+
+    start.resize(T);
+    endi.resize(T);
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            cin >> arr[i][j];
+        }
+    }
+
+    for (int i = 1; i <= N; i++) {
+        for (int j = 1; j <= N; j++) {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+        }
+    }
+
+    for (int i = 0; i < T; i++) {
+        int x1, y1, x2, y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+        //이거 x랑 y의 순서가 이상하지 않나???
+        cout << dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1] << "\n";
+    }
+
+    return 0;
+}

9dde77aa4ba591552b23e7b81b5908f5a0a82a80
syeobnn <48374788+syeobnn@users.noreply.github.com>
2025-02-23 14:43:41 +0900

    [level 4] Title: 5월 식품들의 총매출 조회하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
+++ 프로그래머스/4/131117. 5월 식품들의 총매출 조회하기/5월 식품들의 총매출 조회하기.sql
@@ -1,6 +1,7 @@
 SELECT  fo.product_id, fp.product_name, sum(fp.price * fo.amount) as mach
 FROM    food_product as fp 
         join food_order as fo on fp.product_id = fo.product_id
-WHERE   date_format(fo.produce_date, '%Y-%m') = '2022-05' #%Y 부분에 소문자 꼭 써주기
+WHERE   date_format(fo.produce_date, '%Y-%m') = '2022-05' #%Y 부분에 '' 꼭 써주기
 group by fo.product_id
-order by mach desc, fo.product_id asc;
+order by mach desc, fp.product_id asc; 
+# join을 해줬기 때문에 값만 같으면 정렬할 때 fp.product_id나 fo.product_id나 상관없는듯.

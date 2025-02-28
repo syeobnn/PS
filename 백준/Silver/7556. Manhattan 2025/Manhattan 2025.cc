d0db14bf37482cf8ddfe6cc10f50bf005f849c97
syeobnn <48374788+syeobnn@users.noreply.github.com>
2025-03-01 00:23:51 +0900

    [Silver II] Title: Manhattan 2025, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/7556. Manhattan 2025/Manhattan 2025.cc
+++ 백준/Silver/7556. Manhattan 2025/Manhattan 2025.cc
@@ -1,86 +1,97 @@
+#include <bits/stdc++.h>
 #include <iostream>
-#include <queue>
-#include <tuple>
+
+#define MAX (50000 + 32767)
+#define MIN (50000 - 32768)
+#define log2 18
+#define divisor 1000000000
+#define multiplier 31
+
 using namespace std;
 
-char arr[52][52][52];
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vector<ll> >;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
 
-int ddx[] = {0, 0, 1, -1, 0, 0};   // x축: 오른쪽, 왼쪽
-int ddy[] = {0, 0, 0, 0, -1, 1};    // y축: 위, 아래 (출력 시 행)
-int ddz[] = {1, -1, 0, 0, 0, 0};    // z축: 위쪽, 아래쪽 (slice)
+const int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+const int ddx[] = {0, 0, 1, -1, 0, 0};
+const int ddy[] = {0, 0, 0, 0, -1, 1};
+const int ddz[] = {1, -1, 0, 0, 0, 0};
+const int INF = 0x3f3f3f3f;
+const ll LINF = 3123372036854775807;
 
-void bfs(int fuel) {
-    // 큐브의 크기는 (2*fuel+1)³
-    int cubeSize = 2 * fuel + 1;
-    
-    // cube 영역만 '.'으로 초기화
-    for (int z = 0; z < cubeSize; z++)
-        for (int y = 0; y < cubeSize; y++)
-            for (int x = 0; x < cubeSize; x++)
-                arr[z][y][x] = '.';
-    
-    // 지역 큐를 사용
-    queue<tuple<int, int, int>> q;
-    int start = fuel; // 시작 좌표는 중앙: (fuel, fuel, fuel)
-    q.push({start, start, start});
-    arr[start][start][start] = '0';
-    
-    // 최대 연료를 문자로 저장 ('0'부터 시작하므로)
-    char maxCost = fuel + '0';
-    
+inline void fastIo() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+char arr[52][52][52] = {0};
+queue<tuple<int, int, int> > q;
+int n;
+
+void bfs(int z, int y, int x) {
+    q.push(make_tuple(z, y, x));
+
+    arr[z][y][x] = '0';
+    char stop = z + '0';
+
     while (!q.empty()) {
-        int z, y, x;
-        tie(z, y, x) = q.front();
+        auto [curz, cury, curx] = q.front();
         q.pop();
-        
-        // 이미 최대 연료 소모한 셀은 더 진행하지 않음
-        if (arr[z][y][x] == maxCost)
+
+        if (arr[curz][cury][curx] == stop)
             continue;
-        
-        // 6방향 이동
+
         for (int i = 0; i < 6; i++) {
-            int nz = z + ddz[i];
-            int ny = y + ddy[i];
-            int nx = x + ddx[i];
-            
-            // 경계 검사 (큐브 크기: 0 ~ cubeSize-1)
-            if (nz < 0 || nz >= cubeSize || ny < 0 || ny >= cubeSize || nx < 0 || nx >= cubeSize)
+            int nxtx = curx + ddx[i];
+            int nxty = cury + ddy[i];
+            int nxtz = curz + ddz[i];
+
+            if (nxtx < 0 || nxtx >= 2*z + 1 ||
+                nxty < 0 || nxty >= 2*z + 1 ||
+                nxtz < 0 || nxtz >= 2*z + 1)
                 continue;
-            
-            // 아직 방문하지 않은 셀이면
-            if (arr[nz][ny][nx] == '.') {
-                arr[nz][ny][nx] = arr[z][y][x] + 1; // 비용 1 증가
-                q.push({nz, ny, nx});
+
+            if (arr[nxtz][nxty][nxtx] == '.') {
+                q.push(make_tuple(nxtz, nxty, nxtx));
+                arr[nxtz][nxty][nxtx] = arr[curz][cury][curx] + 1;
             }
         }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    
-    int t;
-    cin >> t;
-    for (int scenario = 1; scenario <= t; scenario++) {
-        int fuel;
-        cin >> fuel;
-        int cubeSize = 2 * fuel + 1;
-        
-        bfs(fuel);
-        
-        cout << "Scenario #" << scenario << ":\n";
-        for (int z = 0; z < cubeSize; z++) {
-            cout << "slice #" << z + 1 << ":\n";
-            for (int y = 0; y < cubeSize; y++) {
-                for (int x = 0; x < cubeSize; x++) {
-                    cout << arr[z][y][x];
+    fastIo();
+
+    cin >> n;
+
+    for (int t = 1; t <= n; t++) {
+        int temp;
+
+        cin >> temp;
+
+        fill(&arr[0][0][0], &arr[0][0][0] + 52*52*52, '.');
+
+        bfs(temp, temp, temp);
+
+        cout << "Scenario #" << t << ":\n";
+
+        for (int i = 0; i < temp * 2 + 1; i++) {
+            cout << "slice #" << i + 1 << ":\n";
+            for (int j = 0; j < temp * 2 + 1; j++) {
+                for (int k = 0; k < temp * 2 + 1; k++) {
+                    cout << arr[i][j][k];
                 }
                 cout << "\n";
             }
         }
+
         cout << "\n";
     }
-    
+
     return 0;
 }

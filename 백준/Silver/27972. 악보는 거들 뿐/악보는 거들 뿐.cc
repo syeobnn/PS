09dd5d2bbfd4934321cbc9d33b18cc2f749eb8f6
syeobnn <48374788+syeobnn@users.noreply.github.com>
2025-03-02 19:29:39 +0900

    [Silver I] Title: 악보는 거들 뿐, Time: 68 ms, Memory: 5928 KB -BaekjoonHub

---
+++ 백준/Silver/27972. 악보는 거들 뿐/악보는 거들 뿐.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+#include <iostream>
+
+#define MAX (50000 + 32767)
+#define MIN (50000 - 32768)
+#define log2 18
+#define divisor 1000000000
+#define multiplier 31
+
+using namespace std;
+
+using ll = long long;
+using vl = vector<ll>;
+using vvl = vector<vector<ll> >;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+
+const int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+const int ddx[] = {0, 0, 1, -1, 0, 0};
+const int ddy[] = {0, 0, 0, 0, -1, 1};
+const int ddz[] = {1, -1, 0, 0, 0, 0};
+const int INF = 0x3f3f3f3f;
+const ll LINF = 3123372036854775807;
+
+inline void fastIo() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+long long asc = 1, desc = 1;
+
+int main() {
+    fastIo();
+
+    vector<long long> arr;
+    int n;
+    long long big = 1;
+
+    cin >> n;
+
+    arr.resize(n);
+
+    for (int i =0;i<n;i++) {
+        cin >> arr[i];
+    }
+
+    for (int i = 0;i<n - 1;i++) {
+        if (arr[i] < arr[i+1]) {
+            asc++;
+            desc = 1;
+        }
+        else if (arr[i] > arr[i + 1]) {
+            desc++;
+            asc = 1;
+        }
+
+        long long bigger = max(desc, asc);
+
+        if (big < bigger)
+            big = bigger;
+    }
+
+    cout << big << "\n";
+
+    return 0;
+}

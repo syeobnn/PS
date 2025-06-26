9ab3c8c6749e3c03d7edc1363ffc9bb8d91a3f85
syeobnn <48374788+syeobnn@users.noreply.github.com>
2025-06-26 10:19:36 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 128 ms, Memory: 16448 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <queue>
+#include <set>
+#include <map>
+#include <stack>
+#include <string.h>
+#include <unordered_set>
+#include <unordered_map>
+
+#define ll long long
+#define pii pair<int, int>
+#define MAX 1e9
+#define log2 18
+
+using namespace std;
+
+const int dx[] = {1, -1, 0, 0, 1, 1, -1, -1};
+const int dy[] = {0, 0, 1, -1, 1, -1, 1, -1};
+const int ddx[] = {1, -1, 0, 0};
+const int ddy[] = {0, 0, -1, 1};
+const int INF = 0x3f3f3f3f;
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+
+inline void fastIo() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+}
+
+int n, m;
+unordered_map<string, string> um_password;
+
+int main() {
+    fastIo();
+
+    cin >> n >> m;
+
+    for (int i =0;i<n;i++) {
+        string site, password;
+        cin >> site >> password;
+
+        um_password[site] = password;
+    }
+
+    for (int i =0;i<m;i++) {
+        string find_password;
+        cin >> find_password;
+        cout << um_password[find_password] << "\n";
+    }
+
+    return 0;
+}
